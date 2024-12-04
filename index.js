d38376f71de7263c88d48ed48569fd60a218d85b
Colack <UniformKitten281@gmail.com>
2024-12-03 20:28:48 -0800

    add index files

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
+app.use((req, res) => {
+    res.status(404).send('Page not found');
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
